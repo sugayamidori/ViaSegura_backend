a5b2b6b526e80ba5a6a4c6bff168f47161e23cc1
pedro.vinicius <pedro.vinicius@educ.rec.br>
2025-09-17 17:59:04 -0300

    Integração com o banco de dados

---
+++ viaseguraapi/src/main/java/com/github/sugayamidori/viaseguraapi/config/DatabaseConfiguration.java
@@ -0,0 +1,45 @@
+package com.github.sugayamidori.viaseguraapi.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Slf4j
+public class DatabaseConfiguration {
+
+    @Value("${spring.datasource.url}")
+    String url;
+    @Value("${spring.datasource.username}")
+    String username;
+    @Value("${spring.datasource.password}")
+    String password;
+    @Value("${spring.datasource.driver-class-name}")
+    String driver;
+
+    @Bean
+    public DataSource hikariDataSource() {
+
+        log.info("Iniciando conexão com o banco na URL: {}", url);
+
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(1);
+        config.setPoolName("library-db-pool");
+        config.setMaxLifetime(600000);
+        config.setConnectionTimeout(100000);
+        config.setConnectionTestQuery("select 1");
+
+        return new HikariDataSource(config);
+    }
+}
